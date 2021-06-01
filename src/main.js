4c8ef763ad327d071eb891606cbe6f42e39ec0fa
chinmay-sheth <chinmay.sheth@hnrtech.com>
2021-06-01 23:04:20 +0530

    client id added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import GAuth from '/src/plugins/google-oauth';
 const app = createApp(App)
-let gauthClientId = "768834812579-ivi0oopbkqe05cg6t41p83t7gteekut6.apps.googleusercontent.com";
+let gauthClientId = "794381519357-7a59g9psl47ut2hjp6d0nv0sa0juifgc.apps.googleusercontent.com";
 app.use(GAuth, { clientId: gauthClientId, scope: 'email', prompt: 'consent', fetch_basic_profile: false })
 app.mount('#app')
